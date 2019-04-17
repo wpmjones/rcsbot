c4c4e7d326b743fb45fe3341e308a64dd87c9538
Patrick Jones <wpmjones@gmail.com>
2019-04-16 19:50:40 -0500

    adding git pull option

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,54 +1,67 @@
 from discord.ext import commands
 
+
 class OwnerCog:
-  def __init__(self, bot):
-    self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-  @commands.command(name='load', hidden=True)
-  @commands.is_owner()
-  async def cog_load(self, ctx, *, cog: str):
-    """Command which loads a module.
-    Remember to use dot path. e.g: cogs.owner"""
+    @commands.command(name="load", hidden=True)
+    @commands.is_owner()
+    async def cog_load(self, ctx, *, cog: str):
+        """Command which loads a module.
+        Remember to use dot path. e.g: cogs.owner"""
 
-    try:
-      self.bot.load_extension(cog)
-    except Exception as e:
-      print(f'ERROR: {type(e).__name__} - {e}')
-      await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
-    else:
-      print(f'{cog} successfully loaded')
-      await ctx.send('**`SUCCESS`**')
+        try:
+            self.bot.load_extension(cog)
+        except Exception as e:
+            print(f"ERROR: {type(e).__name__} - {e}")
+            await ctx.send(f"**`ERROR:`** {type(e).__name__} - {e}")
+        else:
+            print(f"{cog} successfully loaded")
+            await ctx.send("**`SUCCESS`**")
 
-  @commands.command(name='unload', hidden=True)
-  @commands.is_owner()
-  async def cog_unload(self, ctx, *, cog: str):
-    """Command which unloads a module.
-    Remember to use dot path. e.g: cogs.owner"""
+    @commands.command(name="unload", hidden=True)
+    @commands.is_owner()
+    async def cog_unload(self, ctx, *, cog: str):
+        """Command which unloads a module.
+        Remember to use dot path. e.g: cogs.owner"""
 
-    try:
-      self.bot.unload_extension(cog)
-    except Exception as e:
-      print(f'ERROR: {type(e).__name__} - {e}')
-      await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
-    else:
-      print(f'{cog} successfully unloaded')
-      await ctx.send('**`SUCCESS`**')
+        try:
+            self.bot.unload_extension(cog)
+        except Exception as e:
+            print(f"ERROR: {type(e).__name__} - {e}")
+            await ctx.send(f"**`ERROR:`** {type(e).__name__} - {e}")
+        else:
+            print(f"{cog} successfully unloaded")
+            await ctx.send("**`SUCCESS`**")
 
-  @commands.command(name='reload', hidden=True)
-  @commands.is_owner()
-  async def cog_reload(self, ctx, *, cog: str):
-    """Command which reloads a module.
-    Remember to use dot path. e.g: cogs.owner"""
+    @commands.command(name="reload", hidden=True)
+    @commands.is_owner()
+    async def cog_reload(self, ctx, *, cog: str):
+        """Command which reloads a module.
+        Remember to use dot path. e.g: cogs.owner"""
 
-    try:
-      self.bot.unload_extension(cog)
-      self.bot.load_extension(cog)
-    except Exception as e:
-      print(f'ERROR: {type(e).__name__} - {e}')
-      await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
-    else:
-      print(f'{cog} reloaded successfully')
-      await ctx.send('**`SUCCESS`**')
+        try:
+            self.bot.unload_extension(cog)
+            self.bot.load_extension(cog)
+        except Exception as e:
+            print(f"ERROR: {type(e).__name__} - {e}")
+            await ctx.send(f"**`ERROR:`** {type(e).__name__} - {e}")
+        else:
+            print(f"{cog} reloaded successfully")
+            await ctx.send("**`SUCCESS`**")
+
+    @commands.command(name="pull", hidden=True)
+    @commands.is_owner()
+    async def git_pull(self, ctx):
+        """Command to pull latest updates from master branch on GitHub"""
+        origin = self.bot.repo.remotes.origin
+        try:
+            origin.pull()
+            print("Code successfully pulled from GitHub")
+        except Exception as e:
+            print(f"ERROR: {type(e).__name__} - {e}")
+            await ctx.send(f"**`ERROR:`** {type(e).__name__} - {e}")
 
 def setup(bot):
     bot.add_cog(OwnerCog(bot))
--- rcsbot.py
+++ rcsbot.py
@@ -1,4 +1,6 @@
-import discord, requests, pymssql, sys, traceback
+import sys
+import traceback
+import os
 from discord.ext import commands
 from config import settings
 import logging
@@ -6,33 +8,35 @@ from datetime import datetime
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-logger.info('Starting bot')
+logger.info("Starting bot")
 
-description = '''Multi bot to serve the RCS - by TubaKid
+description = """Multi bot to serve the RCS - by TubaKid
 
 For most commands, you will need to include a clan name or tag.
 
 You can use the clan tag (with or without the hashtag) or you can use the clan name (spelling is kind of important).
 
-There are easter eggs. Feel free to try and find them!'''
+There are easter eggs. Feel free to try and find them!"""
 
-bot = commands.Bot(command_prefix='++', description=description, case_insensitive=True)
-bot.remove_command('help')
+bot = commands.Bot(command_prefix="++", description=description, case_insensitive=True)
+bot.remove_command("help")
+bot.repo = git.Repo(os.getcwd())
+
 
 @bot.event
 async def on_ready():
-  print('-------')
-  print('Logged in as {0.user}'.format(bot))
-  print('-------')
+  print("-------")
+  print(f"Logged in as {bot.user}")
+  print("-------")
 
-initialExtensions = ['cogs.general','cogs.push', 'cogs.games', 'cogs.cwl', 'cogs.newhelp', 'cogs.council', 'cogs.owner', 'cogs.eggs']
+initialExtensions = ["cogs.general","cogs.push", "cogs.games", "cogs.newhelp", "cogs.council", "cogs.owner", "cogs.eggs"]
 
-if __name__ == '__main__':
+if __name__ == "__main__":
   for extension in initialExtensions:
     try:
       bot.load_extension(extension)
     except Exception as e:
-      print('Failed to load extension {}'.format(extension), file=sys.stderr)
+      print(f"Failed to load extension {extension}", file=sys.stderr)
       traceback.print_exc()
 
 bot.run(settings['discord']['rcsbotToken'])
