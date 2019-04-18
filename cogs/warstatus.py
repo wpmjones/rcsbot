5e36b438908efff24988fd95caef588507eac70f
W. Patrick Jones <wpmjones@gmail.com>
2019-04-17 23:01:14 -0500

    updating war status script

--- cogs/warstatus.py
+++ cogs/warstatus.py
@@ -1,6 +1,7 @@
 import coc
 import discord
 import asyncio
+import pymssql
 from config import settings
 
 
@@ -11,16 +12,25 @@ class WarStatus:
     async def war_report(self):
         """ For reporting wars to RCS war-updates channel """
         await self.bot.wait_until_ready()
-        count = 1
+        api = coc.Client(settings['supercell']['apiKey'])
         channel = self.bot.get_channel(settings['oakChannels']['testChat'])
         while not self.bot.is_closed():
             seconds_until_post = 300
-            print(f"Sleeping for {seconds_until_post/60} minutes.")
+            print(f"Sleeping for {seconds_until_post / 60} minutes.")
             await asyncio.sleep(seconds_until_post)
 
-            print(f"Counter at {str(count)}")
-            await channel.send(f"Counter at {str(count)}")
-            count += 1
+            conn = pymssql.connect(settings['database']['server'],
+                                   settings['database']['username'],
+                                   settings['database']['password'],
+                                   settings['database']['database'])
+            cursor = conn.cursor()
+            cursor.execute('SELECT clanTag FROM rcs_data ORDER BY clanName')
+            rows = cursor.fetchall()
+            clan_list = []
+            for tag in rows:
+                clan = api.get_clan(tag[0])
+                print(clan.name)
+                await channel.send(clan.name)
 
 
 def setup(bot):
