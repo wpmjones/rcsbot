d9a1b2ab2957c32a2ea600a570803d77124cc8cb
Patrick Jones <wpmjones@gmail.com>
2019-04-18 13:34:12 -0500

    works without coc.py - need to test with maths

--- cogs/warstatus.py
+++ cogs/warstatus.py
@@ -1,4 +1,4 @@
-#import coc
+# import coc
 import discord
 import asyncio
 import pymssql
@@ -9,16 +9,16 @@ class WarStatus:
     def __init__(self, bot):
         self.bot = bot
 
-    # TODO figure out how to close loop before stopping cog
+    # TODO work with mathsman on loop issue
 
     async def war_report(self):
         """ For reporting wars to RCS war-updates channel """
         await self.bot.wait_until_ready()
-        #api = coc.Client(settings['supercell']['apiKey'])
+        # api = coc.Client(settings['supercell']['apiKey'])
         channel = self.bot.get_channel(settings['oakChannels']['testChat'])
         while self == self.bot.get_cog("WarStatus"):
             seconds_until_post = 60
-            print(f"Sleeping for {seconds_until_post / 60} minutes.")
+            print(f"Sleeping for {seconds_until_post % 60} minutes.")
             await asyncio.sleep(seconds_until_post)
 
             conn = pymssql.connect(settings['database']['server'],
@@ -26,11 +26,11 @@ class WarStatus:
                                    settings['database']['password'],
                                    settings['database']['database'])
             cursor = conn.cursor()
-            cursor.execute('SELECT clanTag FROM rcs_data ORDER BY clanName')
+            cursor.execute("SELECT clanTag FROM rcs_data ORDER BY clanName")
             rows = cursor.fetchall()
             conn.close()
             for tag in rows:
-                #clan = api.get_clan(tag[0])
+                # clan = api.get_clan(tag[0])
                 print(tag[0])
                 await channel.send(tag[0])
 
