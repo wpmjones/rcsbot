b4c3c3d7f287c4b2c9ae86e912f5bad23ceb2890
Patrick Jones <wpmjones@gmail.com>
2019-04-19 11:03:15 -0500

    adding avatar feature in eggs

--- cogs/eggs.py
+++ cogs/eggs.py
@@ -6,86 +6,97 @@ from datetime import datetime
 from config import settings, emojis
 
 class Eggs:
-  '''Cog for easter egg commands (guess away)'''
-  def __init__(self, bot):
-    self.bot = bot
+    """Cog for easter egg commands (guess away)"""
+    def __init__(self, bot):
+        self.bot = bot
 
-  @commands.command(name='colors', aliases=['colours','color','colour'], hidden=True)
-  async def colors(self, ctx):
-    botLog(ctx.command,ctx.author)
-    content = "The following commands give you the respective colour:\n"
-    content += "\n**!holipink**"
-    content += "\n**!holired**"
-    content += "\n**!holiorange**"
-    content += "\n**!holiyellow**"
-    content += "\n**!holigreen**"
-    content += "\n**!holiteal**"
-    content += "\n**!holiblue**"
-    content += "\n**!holipurple**"
-    await ctx.send(content)
+    # TODO improve bot_log
 
-  @commands.command(name='dmtest', hidden=True)
-  async def dmtest(self, ctx):
-    botLog(ctx.command,ctx.author)
-    await ctx.author.send(ctx.channel)
+    @commands.command(name="avatar", hidden=True)
+    async def avatar(self, ctx, member):
+        # convert discord mention to user id only
+        if player.startswith("<"):
+            discord_id = "".join(player[2:-1])
+            if discord_id.startswith("!"):
+                discord_id = discord_id[1:]
+        else:
+            await ctx.send(emojis['other']['redx'] + " I don't believe that's a real Discord user. Please make sure you are using the '@' prefix.")
+            return
+        guild = ctx.bot.get_guild(settings['discord']['rcsGuildId'])
+        isUser, user = is_discord_user(guild, int(discord_id))
+        if not isUser:
+            await ctx.send(f"{emojis['other']['redx']} User provided **{member}** is not a member of this discord server.")
+            return
+        await ctx.send(user.avatar)
+        bot_log(ctx.command,ctx.author)
 
-  @commands.command(name='testing', hidden=True)
-  async def testing(self, ctx):
-    botLog(ctx.command,ctx.author)
-    bk = 33
-    aq = 22
-    th = 10
-    await ctx.send('{} {} {} {} {}TubaToo'.format(emojis['th'][th], emojis['level'][bk], emojis['level'][aq], emojis['other']['gap'], emojis['other']['gap']))
+    @commands.command(name="testing", hidden=True)
+    async def testing(self, ctx):
+        bot_log(ctx.command,ctx.author)
+        bk = 33
+        aq = 22
+        th = 10
+        await ctx.send(f"{emojis['th'][th]} {emojis['level'][bk]} {emojis['level'][aq]} {emojis['other']['gap']} {emojis['other']['gap']}TubaToo"
 
-  @commands.command(name='zag', aliases=['zag-geek','zaggeek'], hidden=True)
-  async def zag(self, ctx):
-    botLog(ctx.command,ctx.author)
-    await ctx.send(file=discord.File('/home/tuba/bot/cogs/zag.jpg'))
+                       @commands.command(name="zag", aliases=["zag-geek","zaggeek"], hidden=True)
+        async def zag(self, ctx):
+        bot_log(ctx.command,ctx.author)
+        await ctx.send(file=discord.File("/home/tuba/bot/cogs/zag.jpg"))
 
-  @commands.command(name='tuba', hidden=True)
-  async def tuba(self, ctx):
-    botLog(ctx.command,ctx.author)
-    await ctx.send(file=discord.File('/home/tuba/bot/cogs/tuba.jpg'))
+    @commands.command(name="tuba", hidden=True)
+    async def tuba(self, ctx):
+        bot_log(ctx.command,ctx.author)
+        await ctx.send(file=discord.File("/home/tuba/bot/cogs/tuba.jpg"))
 
-  @commands.command(name='password', hidden=True)
-  async def password(self, ctx):
-    content = 'https://www.reddit.com/r/RedditClansHistory/wiki/the_history_of_the_reddit_clans#wiki_please_find_the_password'
-    botLog(ctx.command,ctx.author)
-    await ctx.send(content)
+    @commands.command(name="password", hidden=True)
+    async def password(self, ctx):
+        content = "https://www.reddit.com/r/RedditClansHistory/wiki/the_history_of_the_reddit_clans#wiki_please_find_the_password"
+        bot_log(ctx.command,ctx.author)
+        await ctx.send(content)
 
-  @commands.command(name='cats', aliases=['cat'], hidden=True)
-  async def kitty(self, ctx):
-    url = 'https://api.thecatapi.com/v1/images/search'
-    headers = {
-      'Content-Type': 'application/json',
-      'x-api-key': settings['api']['catKey']
-    }
-    r = requests.get(url, headers=headers)
-    data = r.json()
-    content = data[0]['url']
-    botLog(ctx.command,ctx.author)
-    await ctx.send(content)
+    @commands.command(name="cats", aliases=["cat"], hidden=True)
+    async def kitty(self, ctx):
+        url = "https://api.thecatapi.com/v1/images/search"
+        headers = {
+            "Content-Type": "application/json",
+            "x-api-key": settings['api']['catKey']
+        }
+        r = requests.get(url, headers=headers)
+        data = r.json()
+        content = data[0]['url']
+        bot_log(ctx.command,ctx.author)
+        await ctx.send(content)
 
-  @commands.command(name='dogs', aliases=['dog'], hidden=True)
-  async def puppy(self, ctx):
-    url = 'https://api.thedogapi.com/v1/images/search'
-    headers = {
-      'Content-Type': 'application/json',
-      'x-api-key': settings['api']['dogKey']
-    }
-    r = requests.get(url, headers=headers)
-    data = r.json()
-    content = data[0]['url']
-    botLog(ctx.command,ctx.author)
-    await ctx.send(content)
+    @commands.command(name="dogs", aliases=["dog"], hidden=True)
+    async def puppy(self, ctx):
+        url = "https://api.thedogapi.com/v1/images/search"
+        headers = {
+            "Content-Type": "application/json",
+            "x-api-key": settings['api']['dogKey']
+        }
+        r = requests.get(url, headers=headers)
+        data = r.json()
+        content = data[0]['url']
+        bot_log(ctx.command,ctx.author)
+        await ctx.send(content)
 
-def botLog(command, author, errFlag=0):
-  msg = str(datetime.now())[:16] + ' - '
-  if errFlag == 0:
-    msg += 'Printing {}. Requested by {}.'.format(command, author)
-  else:
-    msg += 'ERROR: User provided an incorrect argument for {}. Requested by {}.'.format(command, author)
-  print(msg)
+def is_discord_user(guild, discord_id):
+    try:
+        user = guild.get_member(discord_id)
+        if user is None:
+            return False, None
+        else:
+            return True, user
+    except:
+        return False, None
+
+def bot_log(command, author, errFlag=0):
+    msg = str(datetime.now())[:16] + " - "
+    if errFlag == 0:
+        msg += f"Printing {command}. Requested by {author}."
+    else:
+        msg += f"ERROR: User provided an incorrect argument for {command}. Requested by {author}."
+    print(msg)
 
 def setup(bot):
-  bot.add_cog(Eggs(bot))
+    bot.add_cog(Eggs(bot))
