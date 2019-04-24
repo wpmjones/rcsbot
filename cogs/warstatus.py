24ac5a310032642413b865250942f26bd9a8eb34
W. Patrick Jones <wpmjones@gmail.com>
2019-04-24 16:26:01 -0500

    work on cog loop

--- cogs/warstatus.py
+++ cogs/warstatus.py
@@ -14,7 +14,7 @@ class WarStatus:
     async def war_report(self):
         """ For reporting wars to RCS war-updates channel """
         await self.bot.wait_until_ready()
-        # api = coc.Client(settings['supercell']['apiKey'])
+        api = coc.Client(settings['supercell']['user'], settings['supercell']['pass'])
         channel = self.bot.get_channel(settings['oakChannels']['testChat'])
         while self == self.bot.get_cog("WarStatus"):
             seconds_until_post = 60
@@ -27,12 +27,10 @@ class WarStatus:
                                    settings['database']['database'])
             cursor = conn.cursor()
             cursor.execute("SELECT clanTag FROM rcs_data ORDER BY clanName")
-            rows = cursor.fetchall()
+            tags = cursor.fetchall()
             conn.close()
-            for tag in rows:
-                # clan = api.get_clan(tag[0])
-                print(tag[0])
-                await channel.send(tag[0])
+            async for clan_war in api.get_current_wars(tags):
+                await channel.send(f"{clan_war.clan.name}: {clan_war}.state")
 
 
 def setup(bot):
