df365edb74910fa487a2a63f5f2c56851eda1a55
W. Patrick Jones <wpmjones@gmail.com>
2019-04-24 16:45:48 -0500

    moved api declaration

--- cogs/warstatus.py
+++ cogs/warstatus.py
@@ -3,6 +3,8 @@ import asyncio
 import pymssql
 from config import settings
 
+api = coc.Client(settings['supercell']['user'], settings['supercell']['pass'])
+
 
 class WarStatus:
     def __init__(self, bot):
@@ -13,7 +15,6 @@ class WarStatus:
     async def war_report(self):
         """ For reporting wars to RCS war-updates channel """
         await self.bot.wait_until_ready()
-        api = coc.Client(settings['supercell']['user'], settings['supercell']['pass'])
         channel = self.bot.get_channel(settings['oakChannels']['testChat'])
         while self == self.bot.get_cog("WarStatus"):
             seconds_until_post = 60
