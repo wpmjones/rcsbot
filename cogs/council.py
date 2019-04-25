019cef89defb0712df9a23d686b5461ab44abb41
W. Patrick Jones <wpmjones@gmail.com>
2019-04-25 16:12:51 -0500

    testing has_any_roles

--- cogs/council.py
+++ cogs/council.py
@@ -29,7 +29,7 @@ class CouncilCog:
 
     @commands.command(name="userInfo", aliases=["ui"], hidden=True)
     # @commands.check(is_rcs)
-    # @commands.has_any_role(settings['rcsRoles']['council'], settings['rcsRoles']['chatMods'])
+    @commands.has_any_role(566692408033804288, 569279865598115894)  # settings['rcsRoles']['council'], settings['rcsRoles']['chatMods'])
     async def user_info(self, ctx, discord_id):
         """Command to retreive join date for Discord user."""
         is_user, user = is_discord_user(ctx.guild, int(discord_id))
