bec91d2b1eeacf8113b443cc8c1c6c16ad77a669
W. Patrick Jones <wpmjones@gmail.com>
2019-04-26 10:51:28 -0500

    fixing loguru parameters

--- cogs/background.py
+++ cogs/background.py
@@ -5,8 +5,8 @@ from datetime import datetime, timedelta
 from random import randint
 from config import settings
 
-logger.add("general.log", rotation="100MB",
-           format="{time:YYYY-MM-DD HH:mm:ss} {level} {message}", level="INFO")
+# logger.add("general.log", rotation="100MB",
+#            format="{time:YYYY-MM-DD HH:mm:ss} {level} {message}", level="INFO")
 
 
 class Background(commands.Cog):
@@ -24,11 +24,14 @@ class Background(commands.Cog):
             return
         if settings['rcsRoles']['members'] not in message.author.roles:
             return
+        logger.debug("User has member role.")
         conn = await asyncpg.connect(user=settings['pg']['user'],
                                      password=settings['pg']['pass'],
                                      host=settings['pg']['host'],
                                      database=settings['pg']['db'])
+        logger.debug("Postgresql connection established.")
         row = await conn.fetchrow(f"SELECT * FROM rcs_discord WHERE discord_id = {message.author.id}")
+        logger.debug(row)
         points = randint(7, 14)
         if row:
             if datetime.now() > row['last_message'] + timedelta(minutes=1):
@@ -36,10 +39,13 @@ class Background(commands.Cog):
                                    f"SET message_points = {row['message_points']+points}, "
                                    f"last_message = '{datetime.now()}' "
                                    f"WHERE discord_id = {message.author.id}")
+                logger.info("{} receives {} for their message", message.author.display_name, points)
+            else:
+                logger.debug("User posted within the last minute. No points awarded.")
         else:
-            logger.info("Adding {} to the rcs_discord table for message tracking", message.author.display_name)
             await conn.execute(f"INSERT INTO rcs_discord "
                                f"VALUES ({message.author.id}, {points}, 0, '{datetime.now()}')")
+            logger.info("Added {} to the rcs_discord table for message tracking", message.author.display_name)
 
 
 def setup(bot):
