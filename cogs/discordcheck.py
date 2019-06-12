afc9040395b12924d91a75fc57051557a82749ac
W. Patrick Jones <wpmjones@gmail.com>
2019-06-11 20:58:52 -0500

    fix discord check to deal with the massive number of members in 10% Beaver

--- cogs/discordcheck.py
+++ cogs/discordcheck.py
@@ -18,6 +18,12 @@ class DiscordCheck(commands.Cog):
     async def main(self):
         while self.flag == 1:
             start = time.perf_counter()
+            guild = self.bot.get_guild(settings['discord']['rcsGuildId'])
+            danger_channel = guild.get_channel(settings['rcsChannels']['dangerBot'])
+            botdev_channel = guild.get_channel(settings['rcsChannels']['botDev'])
+            notes_channel = guild.get_channel(settings['rcsChannels']['leaderNotes'])
+            mods_channel = guild.get_channel(settings['rcsChannels']['mods'])
+            member_role = guild.get_role(settings['rcsRoles']['members'])
             conn = pymssql.connect(settings['database']['server'],
                                    settings['database']['username'],
                                    settings['database']['password'],
@@ -41,16 +47,15 @@ class DiscordCheck(commands.Cog):
                         clan_list.append(clan)
                 else:
                     clan_list.append(row[0])
-            guild = self.bot.get_guild(settings['discord']['rcsGuildId'])
-            danger_channel = guild.get_channel(settings['rcsChannels']['dangerBot'])
+            self.bot.logger.debug("About the clear messages")
             async for message in danger_channel.history():
                 await message.delete()
-            notes_channel = guild.get_channel(settings['rcsChannels']['leaderNotes'])
-            member_role = guild.get_role(settings['rcsRoles']['members'])
+            self.bot.logger.debug("Messages cleared")
             # THIS IS THE BEGINNING OF THE LEADER NOTE CHECKS
             message_list = []
             async for message in notes_channel.history(limit=None, oldest_first=True):
                 message_list.append(message.content)
+            self.bot.logger.debug("Messages pulled from Leader Notes")
             messages = " - ".join(message_list)
             regex = r"[tT]ag:\s[a-zA-Z0-9]+|#[a-zA-Z0-9]{6,}"
             ban_set = set()
@@ -60,6 +65,7 @@ class DiscordCheck(commands.Cog):
                 else:
                     ban_set.add(match.group())
             ban_list = list(ban_set)
+            self.bot.logger.debug("Starting to loop through ban_list")
             for tag in ban_list:
                 if tag == '#UQRJVPJV':
                     continue   # this is a clan, not a player
@@ -80,17 +86,18 @@ class DiscordCheck(commands.Cog):
                                             f"search for `in:leader-notes {player.tag}` for details.")
                             embed.set_footer(text="Reminder: This is not a ban list, simply information that this "
                                                   "member has caused problems in the past.")
-                            channel = guild.get_channel(settings['rcsChannels']['dangerBot'])
-                            await channel.send(embed=embed)
+                            await danger_channel.send(embed=embed)
                             cursor.execute(f"INSERT INTO rcs_notify "
                                            f"VALUES ({datetime.now().strftime('%m-%d-%Y %H:%M:%S')}, "
                                            f"{player.clan.tag[1:]}, {player.tag[1:]})")
                             conn.commit()
                         conn.close()
                 except:
-                    print("Bad player tag")
+                    self.bot.logger.debug(f"{tag} is not a real player tag.")
             # THIS IS THE BEGINNING OF THE NAME CHECKS
+            self.bot.logger.debug("Beginning of daily clan check")
             for clan in daily_clans:
+                self.bot.logger.debug(f"Checking {clan['clan_name']}")
                 report_list = []
                 short_list = clan['short_name'].split("/")
                 for short_name in short_list:
@@ -98,29 +105,31 @@ class DiscordCheck(commands.Cog):
                         regex = r"\W" + short_name + "\W|\W" + short_name + "\Z"
                     else:
                         regex = r"\Wreddit[^\s]"
+                    self.bot.logger.debug("Looping through Discord members.")
                     for member in guild.members:
                         if member_role in member.roles \
                                 and re.search(regex, member.display_name, re.IGNORECASE) is not None:
-                            report_list.append(f"Discord Name: {member.display_name.replace('||', '|')}")
+                            report_list.append(member.display_name.replace('||', '|'))
+                self.bot.logger.debug(f"Reviewed all members for {clan['clan_name']}")
                 if report_list:
-                    channel = guild.get_channel(settings['rcsChannels']['dangerBot'])
-                    clan_header = f"**Results for {clan['clan_name']}**"
+                    clan_header = f"Results for {clan['clan_name']}"
                     content = ""
                     for entry in report_list:
                         content += f"  {entry}\n"
+                    print(len(content))
                     embed = discord.Embed(color=color_pick(181, 0, 0))
-                    embed.add_field(name=clan_header, value=content)
+                    embed.add_field(name=clan_header, value=content, inline=False)
                     embed.set_footer(text="If someone is no longer in your clan, please notify a Chat Mod "
                                           "to have their Member role removed.",
                                      icon_url="http://www.mayodev.com/images/dangerbot.png")
-                    await channel.send(f"<@{clan['leader_tag']}> Please check the following list of members to make "
-                                       f"sure everyone is still in your clan (or feeder).")
-                    await channel.send(embed=embed)
+                    await danger_channel.send(f"<@{clan['leader_tag']}> Please check the following list of members "
+                                              f"to make sure everyone is still in your clan (or feeder).")
+                    await danger_channel.send(embed=embed)
                     if clan['clan_name'] in ["Ninja Killers", "Faceless Ninjas"]:
                         requests.post(settings['rcsWebhooks']['ninjas'])
                 else:
-                    channel = guild.get_channel(settings['rcsChannels']['botDev'])
-                    await channel.send(f"No members for {clan['clan_name']}")
+                    await botdev_channel.send(f"No members for {clan['clan_name']}")
+            # THIS SECTION CHECKS FOR MEMBERS WITHOUT ANY CLAN AFFILIATION
             if date.today().weekday() == 6:
                 errors = []
                 for member in guild.members:
@@ -136,11 +145,9 @@ class DiscordCheck(commands.Cog):
                     embed = discord.Embed(color=color_pick(181, 0, 0))
                     embed.add_field(name="We found some Members without a clan:",
                                     value="\n  ".join(errors))
-                    channel = guild.get_channel(settings['rcsChannels']['mods'])
-                    await channel.send(embed=embed)
+                    await mods_channel.send(embed=embed)
             elapsed = time.perf_counter() - start
-            channel = guild.get_channel(settings['rcsChannels']['botDev'])
-            await channel.send(f"I'm going to sleep for {((60*60*24) - elapsed):.2f} seconds. See you tomorrow!")
+            await botdev_channel.send(f"I'm going to sleep for {((60*60*24) - elapsed):.2f} seconds. See you tomorrow!")
             conn.close()
             await asyncio.sleep((60*60*24) - elapsed)
 
@@ -149,6 +156,7 @@ class DiscordCheck(commands.Cog):
     async def flip(self, ctx):
         if self.flag == 1:
             self.flag = 0
+            await ctx.send("Flag changed to 0")
 
 
 def setup(bot):
