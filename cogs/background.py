9f2cd07296c363b69d4aa6f240e5d5e427b7cb0d
W. Patrick Jones <wpmjones@gmail.com>
2019-07-13 11:33:23 -0400

    change order of checks to avoid errors on no guild id in DM

--- cogs/background.py
+++ cogs/background.py
@@ -1,4 +1,3 @@
-from loguru import logger
 import discord
 from discord.ext import commands
 from datetime import datetime, timedelta
@@ -17,18 +16,16 @@ class Background(commands.Cog):
             await message.channel.send("Thanks for chatting, but I'm really not set up for that. Perhaps try a "
                                        "command like `++help`.")
             return
-        if message.guild.id != settings['discord']['rcsGuildId']:
+        if message.author == self.bot.user or message.guild.id != settings['discord']['rcsGuildId']:
             return
-        logger.debug("New message received.")
-        if message.author == self.bot.user:
-            return
+        self.bot.logger.debug("New message received.")
         if settings['rcsRoles']['members'] not in [role.id for role in message.author.roles]:
-            logger.debug("No member role.\n{} has the following roles.\n{}",
+            self.bot.logger.debug("No member role.\n{} has the following roles.\n{}",
                          message.author.display_name, message.author.roles)
             return
-        logger.debug("User has member role.")
+        self.bot.logger.debug("User has member role.")
         conn = self.bot.db.pool
-        logger.debug("Postgresql connection established.")
+        self.bot.logger.debug("Postgresql connection established.")
         row = await conn.fetchrow(f"SELECT * FROM rcs_discord WHERE discord_id = {message.author.id}")
         points = randint(7, 14)
         if row:
@@ -38,16 +35,16 @@ class Background(commands.Cog):
                                    f"last_message = '{datetime.now()}', "
                                    f"message_count = {row['message_count']+1} "
                                    f"WHERE discord_id = {message.author.id}")
-                logger.info("{} receives {} for their message", message.author.display_name, points)
+                self.bot.logger.info("{} receives {} for their message", message.author.display_name, points)
             else:
                 await conn.execute(f"UPDATE rcs_discord "
                                    f"SET last_message = '{datetime.now()}' "
                                    f"WHERE discord_id = {message.author.id}")
-                logger.info("{} posted within the last minute. No points awarded.", message.author.display_name)
+                self.bot.logger.info("{} posted within the last minute. No points awarded.", message.author.display_name)
         else:
             await conn.execute(f"INSERT INTO rcs_discord "
                                f"VALUES ({message.author.id}, {points}, 0, '{datetime.now()}', 1)")
-            logger.info("Added {} to the rcs_discord table for message tracking", message.author.display_name)
+            self.bot.logger.info("Added {} to the rcs_discord table for message tracking", message.author.display_name)
 
 
 def setup(bot):
