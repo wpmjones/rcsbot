3537c2fbc5cd2c9f785b7834085eeff3bd1875d5
Patrick Jones <wpmjones@gmail.com>
2019-07-19 09:02:46 -0400

    add command for new clan games

--- cogs/eggs.py
+++ cogs/eggs.py
@@ -55,9 +55,9 @@ class Eggs(commands.Cog):
         cursor = conn.cursor(as_dict=True)
         start_day = int(start_date[8:9])
         end_day = str(start_day+games_length)
-        end_date = start_date[:7] + end_day
+        end_date = start_date[:9] + end_day
         sql = (f"INSERT INTO rcs_events (eventType, startTime, endTime, playerPoints, clanPoints) "
-               f"VALUES (5, {start_date}, {end_date}, {ind_points}, {clan_points})")
+               f"VALUES (5, '{start_date}', '{end_date}', {ind_points}, {clan_points})")
         cursor.execute(sql)
         await ctx.send(sql)
         await ctx.send(f"New games info added to database. {end_date}")
