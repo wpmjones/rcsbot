335f9ac13e21b8a6f5178a9072623d7ba131af43
W. Patrick Jones <wpmjones@gmail.com>
2019-08-24 09:36:52 -0400

    testing games_add

--- cogs/games.py
+++ cogs/games.py
@@ -35,12 +35,9 @@ class Games(commands.Cog):
                                    database=settings['database']['database'])
             conn.autocommit(True)
             cursor = conn.cursor(as_dict=True)
-            try:
-                cursor.execute("SELECT MAX(eventId) FROM rcs_events WHERE eventType = 5")
-                row = cursor.fetchone()
-                event_id = row['eventId']
-            except:
-                self.bot.logger.exception("SELECT failed")
+            cursor.execute("SELECT MAX(eventId) as eventId FROM rcs_events WHERE eventType = 5")
+            row = cursor.fetchone()
+            event_id = row['eventId']
             self.bot.logger.debug(event_id)
             starting_points = player.achievements_dict['Games Champion'].value - games_points
             current_points = player.achievements_dict['Games Champion'].value
