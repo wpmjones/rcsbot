dddc2237429b812cee20af3ee82b04f403c453f5
W. Patrick Jones <wpmjones@gmail.com>
2019-10-26 11:22:12 -0400

    halloween upgrades

--- cogs/halloween.py
+++ cogs/halloween.py
@@ -635,7 +635,8 @@ class Halloween(commands.Cog):
             else:
                 content = f"**{ctx.author.display_name} said:\n**" + content
                 await ctx.author.send(content)
-        await ctx.invoke(self.answer)
+        if ctx.message.content in answers:
+            await ctx.invoke(self.answer)
 
     @commands.command(name="skip", aliases=["next"])
     async def skip(self, ctx):
