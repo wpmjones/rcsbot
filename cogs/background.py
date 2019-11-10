773ab8acf0860aeeff04bc1314ff2888c77d0e93
W. Patrick Jones <wpmjones@gmail.com>
2019-11-10 18:10:48 -0500

    add missing self.

--- cogs/background.py
+++ cogs/background.py
@@ -17,16 +17,50 @@ class Background(commands.Cog):
         self.guild = self.bot.get_guild(settings['discord']['rcsguild_id'])
         self.media_stats = self.guild.get_channel(settings['rcs_channels']['media_stats'])
 
-        self.bot.coc.add_events(on_clan_war_win_streak_change,
-                                on_clan_level_change,
-                                on_clan_war_win_change)
+        self.bot.coc.add_events(self.on_clan_war_win_streak_change,
+                                self.on_clan_level_change,
+                                self.on_clan_war_win_change)
         self.bot.coc.add_clan_update(rcs_tags())
         self.bot.coc.start_updates("clan")
 
     def cog_unload(self):
-        self.bot.coc.remove_events(on_clan_war_win_streak_change,
-                                   on_clan_level_change,
-                                   on_clan_war_win_change)
+        self.bot.coc.remove_events(self.on_clan_war_win_streak_change,
+                                   self.on_clan_level_change,
+                                   self.on_clan_war_win_change)
+
+    async def on_clan_war_win_streak_change(self, old_streak, new_streak, clan):
+        """Watch for changes in war win streak and report to media/stats channel"""
+        self.bot.logger.debug("Start war win streak change")
+        if new_streak >= 5:
+            msg = random.choice["And the wins keep coming! ",
+                                "We've got another streak! ",
+                                "Impressive numbers! ",
+                                ]
+            msg += f"**{clan.name}** has just won another one, bringing their streak to {new_streak}."
+            await self.media_stats.send(msg)
+
+    async def on_clan_level_change(self, old_level, new_level, clan):
+        """Watch for changes in clan level and report to media/stats channel"""
+        self.bot.logger.debug("Start clan level change")
+        msg = f"Please help us in congratulating {clan.name} on reaching Level {new_level}."
+        await self.media_stats.send(msg)
+
+    async def on_clan_war_win_change(self, old_wins, new_wins, clan):
+        """Watch for war wins divisible by 50 and report to media/stats channel"""
+        self.bot.logger.debug("Start war win div 50 change")
+        if new_wins % 50 == 0:
+            prefix = random.choice(["Holy smokes, that is a lot of wins! ",
+                                    "Check this out! ",
+                                    "Milestone! ",
+                                    "Hot diggity dog! ",
+                                    "Want to see something cool? "
+                                    "And the wins keep coming! "])
+            suffix = random.choice(["You are awesome!",
+                                    "Keep up the great work!",
+                                    "You're making us all proud!"
+                                    "Go win a few more!"])
+            msg = f"{prefix}{clan.name} just hit **{new_wins}** wins! {suffix}"
+            await self.media_stats(msg)
 
     @tasks.loop(time=time(hour=17))
     async def clan_checks(self):
@@ -78,63 +112,32 @@ class Background(commands.Cog):
                                  icon_url="https://coc.guide/static/imgs/gear_up.png")
                 await council_chat.send(embed=embed)
 
-    async def on_clan_war_win_streak_change(self, old_streak, new_streak, clan):
-        """Watch for changes in war win streak and report to media/stats channel"""
-        if new_streak >= 5:
-            msg = random.choice["And the wins keep coming! ",
-                                "We've got another streak! ",
-                                "Impressive numbers! ",
-                                ]
-            msg += f"**{clan.name}** has just won another one, bringing their streak to {new_streak}."
-            await self.media_stats.send(msg)
-
-    async def on_clan_level_change(self, old_level, new_level, clan):
-        """Watch for changes in clan level and report to media/stats channel"""
-        msg = f"Please help us in congratulating {clan.name} on reaching Level {new_level}."
-        await self.media_stats.send(msg)
-
-    async def on_clan_war_win_change(self, old_wins, new_wins, clan):
-        """Watch for war wins divisible by 50 and report to media/stats channel"""
-        if new_wins % 50 == 0:
-            prefix = random.choice(["Holy smokes, that is a lot of wins! ",
-                                    "Check this out! ",
-                                    "Milestone! ",
-                                    "Hot diggity dog! ",
-                                    "Want to see something cool? "
-                                    "And the wins keep coming! "])
-            suffix = random.choice(["You are awesome!",
-                                    "Keep up the great work!",
-                                    "You're making us all proud!"
-                                    "Go win a few more!"])
-            msg = f"{prefix}{clan.name} just hit **{new_wins}** wins! {suffix}"
-            await self.media_stats(msg)
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        """Awards random points to members when posting messages"""
-        if isinstance(message.channel, discord.DMChannel) and message.author != self.bot.user:
-            return
-        if message.author.bot or message.guild.id != settings['discord']['rcsGuildId']:
-            return
-        if settings['rcsRoles']['members'] not in [role.id for role in message.author.roles]:
-            return
-        conn = self.bot.pool
-        row = await conn.fetchrow(f"SELECT * FROM rcs_discord WHERE discord_id = {message.author.id}")
-        points = randint(7, 14)
-        if row:
-            if datetime.now() > row['last_message'] + timedelta(minutes=1):
-                await conn.execute(f"UPDATE rcs_discord "
-                                   f"SET message_points = {row['message_points']+points}, "
-                                   f"last_message = '{datetime.now()}', "
-                                   f"message_count = {row['message_count']+1} "
-                                   f"WHERE discord_id = {message.author.id}")
-            else:
-                await conn.execute(f"UPDATE rcs_discord "
-                                   f"SET last_message = '{datetime.now()}' "
-                                   f"WHERE discord_id = {message.author.id}")
-        else:
-            await conn.execute(f"INSERT INTO rcs_discord "
-                               f"VALUES ({message.author.id}, {points}, 0, '{datetime.now()}', 1)")
+    # @commands.Cog.listener()
+    # async def on_message(self, message):
+    #     """Awards random points to members when posting messages"""
+    #     if isinstance(message.channel, discord.DMChannel) and message.author != self.bot.user:
+    #         return
+    #     if message.author.bot or message.guild.id != settings['discord']['rcsGuildId']:
+    #         return
+    #     if settings['rcsRoles']['members'] not in [role.id for role in message.author.roles]:
+    #         return
+    #     conn = self.bot.pool
+    #     row = await conn.fetchrow(f"SELECT * FROM rcs_discord WHERE discord_id = {message.author.id}")
+    #     points = randint(7, 14)
+    #     if row:
+    #         if datetime.now() > row['last_message'] + timedelta(minutes=1):
+    #             await conn.execute(f"UPDATE rcs_discord "
+    #                                f"SET message_points = {row['message_points']+points}, "
+    #                                f"last_message = '{datetime.now()}', "
+    #                                f"message_count = {row['message_count']+1} "
+    #                                f"WHERE discord_id = {message.author.id}")
+    #         else:
+    #             await conn.execute(f"UPDATE rcs_discord "
+    #                                f"SET last_message = '{datetime.now()}' "
+    #                                f"WHERE discord_id = {message.author.id}")
+    #     else:
+    #         await conn.execute(f"INSERT INTO rcs_discord "
+    #                            f"VALUES ({message.author.id}, {points}, 0, '{datetime.now()}', 1)")
 
 
 def setup(bot):
