f9248f32a647888e8c2bb3f288eb4cba471edaac
W. Patrick Jones <wpmjones@gmail.com>
2019-11-14 17:10:10 -0500

    disable psql options due to db issue

--- cogs/utils/context.py
+++ cogs/utils/context.py
@@ -188,45 +188,45 @@ class Context(commands.Context):
         emoji = self.tick(opt)
         await self.message.add_reaction(emoji)
 
-    @property
-    def db(self):
-        return self._db if self._db else self.pool
-
-    async def _acquire(self, timeout):
-        if self._db is None:
-            self._db = await self.pool.acquire(timeout=timeout)
-        return self._db
-
-    def acquire(self, *, timeout=None):
-        """Acquires a database connection from the pool. e.g. ::
-
-            async with ctx.acquire():
-                await ctx.db.execute(...)
-
-        or: ::
-
-            await ctx.acquire()
-            try:
-                await ctx.db.execute(...)
-            finally:
-                await ctx.release()
-        """
-        return _ContextDBAcquire(self, timeout)
-
-    async def release(self):
-        """Releases the database connection from the pool.
-
-        Useful if needed for "long" interactive commands where
-        we want to release the connection and re-acquire later.
-
-        Otherwise, this is called automatically by the bot.
-        """
-        # from source digging asyncpg source, releasing an already
-        # released connection does nothing
-
-        if self._db is not None:
-            await self.bot.pool.release(self._db)
-            self._db = None
+    # @property
+    # def db(self):
+    #     return self._db if self._db else self.pool
+    #
+    # async def _acquire(self, timeout):
+    #     if self._db is None:
+    #         self._db = await self.pool.acquire(timeout=timeout)
+    #     return self._db
+    #
+    # def acquire(self, *, timeout=None):
+    #     """Acquires a database connection from the pool. e.g. ::
+    #
+    #         async with ctx.acquire():
+    #             await ctx.db.execute(...)
+    #
+    #     or: ::
+    #
+    #         await ctx.acquire()
+    #         try:
+    #             await ctx.db.execute(...)
+    #         finally:
+    #             await ctx.release()
+    #     """
+    #     return _ContextDBAcquire(self, timeout)
+    #
+    # async def release(self):
+    #     """Releases the database connection from the pool.
+    #
+    #     Useful if needed for "long" interactive commands where
+    #     we want to release the connection and re-acquire later.
+    #
+    #     Otherwise, this is called automatically by the bot.
+    #     """
+    #     # from source digging asyncpg source, releasing an already
+    #     # released connection does nothing
+    #
+    #     if self._db is not None:
+    #         await self.bot.pool.release(self._db)
+    #         self._db = None
 
     async def show_help(self, command=None):
         """Shows the help command for the specified command if given.
