6e3966428ce32754030b9de4715f5d49e6f4bb01
wpmjones@gmail.com <8751ImpalaMustang!>
2019-11-19 18:24:34 -0500

    change server_list command

--- cogs/owner.py
+++ cogs/owner.py
@@ -78,10 +78,10 @@ class OwnerCog(commands.Cog):
     async def server_list(self, ctx):
         """Displays a list of all guilds on which the bot is installed
         Bot owner only"""
-        guild_list = []
+        guild_list = ""
         for guild in self.bot.guilds:
-            guild_list.extend(f"{guild.name} - {guild.id}")
-        await ctx.send("\n".join(guild_list))
+            guild_list += f"{guild.name} - {guild.id}\n"
+        await ctx.send(guild_list)
 
     @commands.command(name="getroles", hidden=True)
     @commands.is_owner()
