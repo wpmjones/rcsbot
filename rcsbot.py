d2e349320d9301ccd274bb23a58286e6e17a75dd
W. Patrick Jones <wpmjones@gmail.com>
2020-01-18 15:32:07 -0500

    fix enviro

--- rcsbot.py
+++ rcsbot.py
@@ -15,7 +15,7 @@ from datetime import datetime
 from config import settings
 from loguru import logger
 
-enviro = "home"
+enviro = "LIVE"
 
 if enviro == "LIVE":
     token = settings['discord']['rcsbot_token']
