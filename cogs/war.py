763bc1cbe0855d1592f9cbe4035dee850dcd423f
W. Patrick Jones <wpmjones@gmail.com>
2020-04-26 09:22:19 -0400

    add testing for on_war_state_change

---
+++ cogs/war.py
@@ -0,0 +1,34 @@
+from discord.ext import commands
+from cogs.utils.helper import rcs_tags
+from config import settings
+
+
+class TestWarUpdates(commands.Cog, command_attrs=dict(hidden=True)):
+    def __init__(self, bot):
+        self.bot = bot
+        self.bot.coc.add_events(self.on_war_state_change)
+        self.bot.coc.add_war_update(rcs_tags())
+        self.bot.coc.start_updtes("war")
+
+    async def on_war_state_change(self, current_state, war):
+        channel = self.bot.get_channel(settings['api']['rcs_demo'])
+        if current_state == "preparation":
+            await channel.send(f"Preparation has just begun for a war between **{war.clan.name}** and "
+                               f"**{war.opponent.name}**.")
+        elif current_state == "inWar":
+            await channel.send(f"War has just begun between **{war.clan.name}** and **{war.opponent.name}**.")
+        elif current_state == "warEnded":
+            content = f"War has just ended between **{war.clan.name}** and **{war.opponent.name}**.\n"
+            if war.status == "won":
+                content += f"{war.clan.name} won!"
+            elif war.status == "lost":
+                content += f"{war.clan.name} lost! :("
+            else:
+                content += "Looks like a tie!"
+            await channel.send(content)
+        else:
+            await channel.send(f"War state is {current_state} for ????")
+
+
+def setup(bot):
+    bot.add_cog(TestWarUpdates(bot))
