90148540d524f03744240a3f2c9108998ff7725d
W. Patrick Jones <wpmjones@gmail.com>
2020-08-07 19:36:03 -0700

    Step 1: Convert to pyodbc (pymssql deprecation)

--- cogs/utils/helper.py
+++ cogs/utils/helper.py
@@ -16,14 +16,14 @@ def get_link_token(maxsize=1):
 @lru_cache(maxsize=4)
 def rcs_names_tags():
     """Retrieve and cache all RCS clan names and tags"""
-    with Sql(as_dict=True) as cursor:
+    with Sql() as cursor:
         sql = "SELECT clanName, clanTag, altName FROM rcs_data ORDER BY clanName"
         cursor.execute(sql)
         fetch = cursor.fetchall()
     clans = {}
     for clan in fetch:
-        clans[clan['clanName'].lower()] = clan['clanTag']
-        clans[clan['altName'].lower()] = clan['clanTag']
+        clans[clan.clanName.lower()] = clan.clanTag
+        clans[clan.altName.lower()] = clan.clanTag
     return clans
 
 
@@ -34,20 +34,20 @@ def rcs_tags(prefix=False):
         field = "'#' + clanTag as tag"
     else:
         field = "clanTag as tag"
-    with Sql(as_dict=True) as cursor:
+    with Sql() as cursor:
         sql = f"SELECT {field} FROM rcs_data ORDER BY clanName"
         cursor.execute(sql)
         fetch = cursor.fetchall()
     clans = []
     for clan in fetch:
-        clans.append(clan['tag'])
+        clans.append(clan.tag)
     return clans
 
 
 @lru_cache(maxsize=64)
 def get_clan(tag):
     """Retrieve the details of a specific clan (provide clan tag without hashtag)"""
-    with Sql(as_dict=True) as cursor:
+    with Sql() as cursor:
         sql = ("SELECT clanName, subReddit, clanLeader, cwlLeague, discordServer, feeder, "
                "classification, discordTag as leaderTag "
                "FROM rcs_data "
@@ -62,11 +62,11 @@ def get_emoji_url(emoji_id):
 
 
 def get_active_wars():
-    with Sql(as_dict=True) as cursor:
+    with Sql() as cursor:
         sql = "SELECT '#' + clanTag as tag, war_id FROM rcs_wars WHERE warState <> 'warEnded'"
         cursor.execute(sql)
         fetch = cursor.fetchall()
     wars = {}
     for row in fetch:
-        wars[row['tag']] = row['war_id']
+        wars[row.tag] = row.war_id
     return wars
