4980c97b4c94bef0a1e4bb09276312e3103b8232
Patrick <wpmjones@gmail.com>
2020-08-30 13:22:08 -0700

    updates to draft cog

--- cogs/draft.py
+++ cogs/draft.py
@@ -13,28 +13,25 @@ class Draft(commands.Cog):
     @commands.has_role("TDW Leaders")
     async def draft(self, ctx, user: discord.Member):
         print("ok")
-        draft_roles = [settings['rcsRoles']['innuendo'],
-                       settings['rcsRoles']['aardvark'],
-                       settings['rcsRoles']['foxtrot'],
-                       settings['rcsRoles']['heroes']
+        draft_roles = [settings['rcs_roles']['innuendo'],
+                       settings['rcs_roles']['aardvark'],
+                       settings['rcs_roles']['foxtrot'],
+                       settings['rcs_roles']['heroes']
                        ]
-        print(draft_roles)
         team_role = None
-        counter = 0
         try:
             print(ctx.author.roles)
             for role in ctx.author.roles:
                 print(role)
                 if role.id in draft_roles:
-                    team_role = role.id
-                    draft_roles.pop(counter)
+                    team_role = int(role.id)
+                    draft_roles.remove(role.id)
                     break
-                counter += 1
             if not team_role:
                 return await ctx.send(f"Hey {ctx.author.name}!  You don't have any team roles yet. Get a team role, "
                                       f"then try again.  K?")
-            guild = ctx.bot.get_guild(settings['discord']['rcsGuildId'])
-            role_obj = guild.get_role(int(team_role))
+            guild = ctx.bot.get_guild(settings['discord']['rcsguild_id'])
+            role_obj = guild.get_role(team_role)
             for role in user.roles:
                 if role.id in draft_roles:
                     return await ctx.send(f"{user.display_name} already has the {role.name} role. To change teams, "
@@ -47,12 +44,12 @@ class Draft(commands.Cog):
     @commands.command(name="undraft")
     @commands.has_role("TDW Leaders")
     async def undraft(self, ctx, user: discord.Member):
-        draft_roles = [settings['rcsRoles']['innuendo'],
-                       settings['rcsRoles']['aardvark'],
-                       settings['rcsRoles']['foxtrot'],
-                       settings['rcsRoles']['heroes']
+        draft_roles = [settings['rcs_roles']['innuendo'],
+                       settings['rcs_roles']['aardvark'],
+                       settings['rcs_roles']['foxtrot'],
+                       settings['rcs_roles']['heroes']
                        ]
-        guild = ctx.bot.get_guild(settings['discord']['rcsGuildId'])
+        guild = ctx.bot.get_guild(settings['discord']['rcsguild_id'])
         for role in draft_roles:
             role_obj = guild.get_role(int(role))
             await user.remove_roles(role_obj, reason=f"Role removed through undraft command "
