0b4eb3c3dd7094e8594167cb0eb4c5fe7cae0965
Patrick <wpmjones@gmail.com>
2021-02-02 11:54:47 -0800

    update games and cwl when season updates

--- cogs/new_season.py
+++ cogs/new_season.py
@@ -42,10 +42,43 @@ class SeasonConfig(commands.Cog, command_attrs=dict(hidden=True)):
     @coc.ClientEvents.new_season_start()
     async def coc_new_season(self):
         """Just testing new_season_start"""
+        season_start = utils.get_season_start()
+        season_end = utils.get_season_end()
         self.bot.logger.info(f"coc has detected a new season\n"
-                             f"{utils.get_season_start()}\n"
-                             f"{utils.get_season_end()}")
-        update_season(utils.get_season_start(), utils.get_season_end())
+                             f"{season_start}\n"
+                             f"{season_end}")
+        update_season(season_start, season_end)
+        # Add next games
+        season_month = season_end.month
+        season_year = season_end.year
+        try:
+            games_start = datetime(season_year, season_month, 22, 8, 0, 0)
+            games_end = datetime(season_year, season_month, 28, 8, 0, 0)
+            if season_month != 8:
+                player_points = 4000
+                clan_points = 50000
+            else:
+                player_points = 5000
+                clan_points = 75000
+            sql = ("INSERT INTO rcs_events (event_type, start_time, end_time, player_points, clan_points) "
+                   "VALUES (1, $1, $2, $3, $4)")
+            await self.bot.pool.execute(sql, games_start, games_end, player_points, clan_points)
+        except:
+            self.bot.logger.exception("Add games failed")
+        # add next CWL
+        try:
+            cwl_start = datetime(season_year, season_month, 1, 3, 0, 0)
+            cwl_end = datetime(season_year, season_month, 10, 3, 0, 0)
+            if len(str(season_month)) == 1:
+                season_month = f"0{str(season_month)}"
+            else:
+                season_month = str(season_month)
+            cwl_season = f"{season_year}-{season_month}"
+            sql = ("INSERT INTO rcs_events (event_type, start_time, end_time, season) "
+                   "VALUES (2, $1, $2, $3")
+            await self.bot.pool.execute(sql, cwl_start, cwl_end, cwl_season)
+        except:
+            self.bot.logger.exception("Add CWL failed")
 
 
 def setup(bot):
