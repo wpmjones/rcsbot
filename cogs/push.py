aef8b30be81d4eae010f0f2e1de5606413edc277
Patrick <wpmjones@gmail.com>
2021-11-20 21:04:12 -0800

    testing push

--- cogs/push.py
+++ cogs/push.py
@@ -16,12 +16,12 @@ class Push(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.title = "2021 Turkey Time Trophy Push"
-        self.start_time = datetime(2021, 11, 21, 5, 0)
+        self.start_time = datetime(2021, 11, 22, 5, 0)
         self.end_time = datetime(2021, 11, 28, 4, 55)
-        # self.update_push.start()
+        self.update_push.start()
 
-    # def cog_unload(self):
-    #     self.update_push.cancel()
+    def cog_unload(self):
+        self.update_push.cancel()
 
     @tasks.loop(minutes=12)
     async def update_push(self):
@@ -62,9 +62,9 @@ class Push(commands.Cog):
                     self.bot.logger.exception(f"Failed on {player_tags[counter]}")
             print("push update complete")
 
-    # @update_push.before_loop
-    # async def before_update_push(self):
-    #     await self.bot.wait_until_ready()
+    @update_push.before_loop
+    async def before_update_push(self):
+        await self.bot.wait_until_ready()
 
     @commands.group(name="push", invoke_without_command=True)
     async def push(self, ctx):
